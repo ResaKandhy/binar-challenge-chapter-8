2c94b015a0a6dcd3687e3d0a23867edd5b49970f
ResaKandhy <resa.kandhy@gmail.com>
2023-03-07 02:41:03 +0800

    Ch8_ Challange

--- server/routes/v1/index.js
+++ server/routes/v1/index.js
@@ -1,10 +1,12 @@
 const playerRouter = require("./player.routes");
 const v1 = require("express").Router();
+const swaggerJSON = require("../v1/swagger.json");
+const swaggerUI = require("swagger-ui-express");
 
 v1.get("/", (_, res) => {
   res.send("from v1");
 });
 
 v1.use("/players", playerRouter);
-
+v1.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerJSON));
 module.exports = v1;
